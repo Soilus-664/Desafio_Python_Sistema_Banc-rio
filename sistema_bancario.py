df06afab66655a7ebf75861918458016b15a1e08
Soilus-664 <marcosadrian450@gmail.com>
2024-10-02 17:12:59 -0300

    Criacao da funcao para o extrato e insercao dos valores dentro da variavel extrato

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -11,19 +11,34 @@ menu = """
     [Q] Sair
 
 => """
+def menu_extrato():
+    if extrato == "":
+        print(f"""
+====================
+Nada Efetuado
+====================
+""")
+    else:
+        print(f"""
+====================\n
+{extrato}
+====================
+""")
+    
 
 def deposito():
-    global saldo
+    global saldo, extrato, exibir_extrato
     deposito = float(input("insira o Valor do Deposito: "))
     
     if deposito > 0:
         saldo += deposito
         print(f"Deposito de {deposito:.2f} efetuado com sucesso!")
+        extrato += f"Depósito: R${deposito}\n"
     else:
         print("A operação Falhou!")
 
 def saque():
-    global limite_saque_diario, SAQUE_DIARIO, saques, saldo
+    global limite_saque_diario, SAQUE_DIARIO, saques, saldo, extrato,exibir_extrato
     
     saque = float(input("insira o Valor do Saque: "))
 
@@ -32,9 +47,10 @@ def saque():
             saldo -= saque
             print(f"Seu Saque de {saque:.2f} foi realizado com sucesso!")
             saques += 1
+            extrato += f"Saque: R${saque}\n"
         elif saque > saldo:
             print(f"Seu Saque de {saque:.2f} é maior que o seu Saldo {saldo:.2f}")
-    elif saque >= limite_saque_diario:
+    elif saque >= limite_saque_diario and saques != SAQUE_DIARIO:
         print(f"O valor de Saque {saque:.2f} é maior que o Limite Diario {limite_saque_diario}")
     elif saques == SAQUE_DIARIO:
         print("Voce excedeu o seu limite de Saques diarios")
@@ -49,8 +65,8 @@ while True:
     elif opcao == "S":
         saque()
     elif opcao == "E":
-        print("Extrato")
-
+        menu_extrato()
+        print(input("aperte [ENTER] para voltar ao menu"))
     elif opcao == "Q":
         break
 
