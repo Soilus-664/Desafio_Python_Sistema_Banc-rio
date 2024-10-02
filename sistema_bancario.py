693b450c07c6d7e35f8188e5b98a71cc9bee3465
Soilus-664 <marcosadrian450@gmail.com>
2024-10-02 16:32:19 -0300

    Criacao da funcao saque e correcao do tipo de entrada na funcao deposito

--- sistema_bancario.py
+++ sistema_bancario.py
@@ -1,5 +1,5 @@
 saldo = 0
-limite_saque = 500 #por Dia
+limite_saque_diario = 500 #por Dia
 SAQUE_DIARIO = 3
 saques = 0
 extrato = ""
@@ -14,19 +14,40 @@ menu = """
 
 def deposito():
     global saldo
-    deposito = int(input("insira o Valor do Deposito: "))
-    saldo += deposito
-    print(f"Deposito de {deposito} efetuado com sucesso!")
+    deposito = float(input("insira o Valor do Deposito: "))
+    
+    if deposito > 0:
+        saldo += deposito
+        print(f"Deposito de {deposito:.2f} efetuado com sucesso!")
+    else:
+        print("A operação Falhou!")
+
+def saque():
+    global limite_saque_diario, SAQUE_DIARIO, saques, saldo
+    
+    saque = float(input("insira o Valor do Saque: "))
+
+    if saque > 0 and saque <= limite_saque_diario and saques < SAQUE_DIARIO:
+        if saque <= saldo:
+            saldo -= saque
+            print(f"Seu Saque de {saque:.2f} foi realizado com sucesso!")
+            saques += 1
+        elif saque > saldo:
+            print(f"Seu Saque de {saque:.2f} é maior que o seu Saldo {saldo:.2f}")
+    elif saque >= limite_saque_diario:
+        print(f"O valor de Saque {saque:.2f} é maior que o Limite Diario {limite_saque_diario}")
+    elif saques == SAQUE_DIARIO:
+        print("Voce excedeu o seu limite de Saques diarios")
+    else:
+        print("A operação Falhou!")
 
 while True:
 
     opcao = input(menu).upper()
     if opcao == "D":
         deposito()
-
     elif opcao == "S":
-        print("insira o Valor do Saque: ")
-
+        saque()
     elif opcao == "E":
         print("Extrato")
 
